c06a11e04f388ad53e6a76a819423bc3e1b4dc2b
TrevKro <trevkro14@gmail.com>
2022-09-14 16:33:48 -0600

    Update client.py

--- client.py
+++ client.py
@@ -3,16 +3,18 @@
 import sys
 import socket
 
-IP = sys.argv[1:]
-PORT = sys.argv[2:]
-FILE = sys.argv [3:]
-SIZE = 1024
+Ip = sys.argv[1]
+Port = sys.argv[2]
+File = sys.argv[3]
+Size = 1024
 
+print(sys.argv)
+print(Ip)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 print(sock)
 
-sock.bind((IP, PORT))
+sock.bind((Ip, Port))
 print(sock)
 
 sock.listen(1)
@@ -21,7 +23,7 @@ clientSocket, clientAddress = sock.accept()
 
 print("Accepted Connection form", clientAddress)
 
-data = clientSocket.recv(SIZE)
+data = clientSocket.recv(Size)
 if data:
     print("Received bytes:", len(data))
     l = clientSocket.send(data)
