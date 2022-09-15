99cc1fbcd67aa1fe62580d2717695aa657f6b28b
TrevKro <trevkro14@gmail.com>
2022-09-14 18:01:48 -0600

    Update client.py

--- client.py
+++ client.py
@@ -9,19 +9,17 @@ fileName = sys.argv[3]
 bufferSize = 4096 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print("Connecting to "+ str(host) + ":" + str(port))
-sock.connect((host, int(port)))
-print("Connected.")
 
-f = open(fileName,'rb')
-print("file opened")
-data = f.read()
-sock.sendfile(f)
-print("file sent. Closing connection")
-f.close()
-sock.close()
-
-
-# host = "131.94.128.43"
-# port = 54634
-# fileName = "file.txt"
+if int(port) > 0 and int(port) > 65535:
+    print("ERROR: port number is out off range 0-65535")
+else:
+    print("Connecting to "+ str(host) + ":" + str(port))
+    sock.connect((host, int(port)))
+    print("Connected.")
+    f = open(fileName,'rb')
+    print("file opened")
+    data = f.read()
+    sock.sendfile(f)
+    print("file sent. Closing connection")
+    f.close()
+    sock.close()
