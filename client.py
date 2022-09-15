477c812fcffca30450e410899f8ef7975f6fd90a
TrevKro <trevkro14@gmail.com>
2022-09-14 19:36:32 -0600

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,7 @@
 import sys
 import socket
 
-host = sys.argv[1]
+host = socket.gethostbyname(sys.argv[1])
 port = sys.argv[2]
 fileName = sys.argv[3]
 bufferSize = 4096 
@@ -14,12 +14,17 @@ if int(port) > 0 and int(port) > 65535:
     print("ERROR: port number is out off range 0-65535")
 else:
     print("Connecting to "+ str(host) + ":" + str(port))
+    #print(sock)
     sock.connect((host, int(port)))
-    print("Connected.")
+    #print(sock)
+    sock.send(b"accio\r\n")
+    sock.send(b"accio\r\n")
+    dataFromServer = sock.recv(1024)
+    print(dataFromServer.decode())
     f = open(fileName,'rb')
     print("file opened")
     data = f.read()
     sock.sendfile(f)
-    print("file sent. Closing connection")
+    print("file sent. Closing connection.")
     f.close()
     sock.close()
