7c85052419e1516a236e568bfdbb0ed07657ee6e
TrevKro <trevkro14@gmail.com>
2022-09-24 18:16:37 -0600

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 import sys
+import socket
+port = sys.argv[1]
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(sock)
+if int(port) > 0 and int(port) > 65535:
+    sys.stderr.write("ERROR: port number is out off range 0-65535\n")
+    sys.exit(1)
+else:    
+    sock.bind(("0.0.0.0", port))
+    sock.listen(10)
+    clientSocket, clientAddress = sock.accept()
+    print("Accepted connection from", clientAddress)
+    data = clientSocket.recv(1024)
+    if data:
+        print("received bytes:", len(data))
+        len = clientSocket.send(data)
+        print("send bytes: %d" % len)
 
-if __name__ == '__main__':
-    sys.stderr.write("server is not implemented yet\n")
+    clientSocket.close()
+    sock.close()
