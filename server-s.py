655328163589d6543ecdd4955dd8442f34bab16a
TrevKro <trevkro14@gmail.com>
2022-09-25 17:18:35 -0600

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -6,31 +6,26 @@ port = sys.argv[1]
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(sock)
 if int(port) > 0 and int(port) > 65535:
+  
     sys.stderr.write("ERROR: port number is out off range 0-65535\n")
     sys.exit()
 else:    
     sock.bind(("0.0.0.0", int(port)))
     sock.listen(10)
+    clientSocket, clientAddress = sock.accept()
+    print("Accepted connection from", clientAddress)
+    sock.setblocking(False)
     sock.send("accio")
     if sock.recv(1024) == b'confirm-accio\r\n':
         sock.send("accio")
         if sock.recv(1024) == b'confirm-accio\r\n\r\n':
-            clientSocket, clientAddress = sock.accept()
-            print("Accepted connection from", clientAddress)
-            data = clientSocket.recv(1024)
-            #if data:
-             #   print("received bytes:", len(data))
-              #  len = clientSocket.send(data)
-               # print("send bytes: %d" % len)
-            total_size = 0
-            while True:
-                block = sock.recv(1024*1024)
-                if not block:
-                    break
-                print('Size of block: {}'.format(len(block)))
-                total_size += len(block)
 
-            print('Total size: {}'.format(total_size))
+            data = clientSocket.recv(1024)
+            if data:
+                print("received bytes:", len(data))
+                len = clientSocket.send(data)
+                print("send bytes: %d" % len)
+            sock.close()
         else:
             sys.exit()
     else:
