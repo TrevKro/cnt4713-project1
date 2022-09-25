d06b978ad702ef2daf5aac11c5cc3f26eac38773
TrevKro <trevkro14@gmail.com>
2022-09-24 18:36:04 -0600

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -7,10 +7,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print(sock)
 if int(port) > 0 and int(port) > 65535:
     sys.stderr.write("ERROR: port number is out off range 0-65535\n")
-    sys.exit(1)
+    sys.exit()
 else:    
     sock.bind(("0.0.0.0", port))
-    sock.listen(10)
+    sock.listen(1)
     clientSocket, clientAddress = sock.accept()
     print("Accepted connection from", clientAddress)
     data = clientSocket.recv(1024)
