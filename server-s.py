e0fc9a262e43b9da5c4c6b56c38acff5bb4b5dba
TrevKro <trevkro14@gmail.com>
2022-09-25 19:04:46 -0600

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -1,34 +1,4 @@
-#!/usr/bin/env python3
-class ClientData:
-    def __init__(self, addr):
-        self.addr = addr
-        self.inb = b'' # input buffer
-        self.outb = b'' # output buffer
 
-def acceptConnection(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print('accepted connection from', addr)
-    conn.setblocking(False)
-    data = ClientData(addr)
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-
-def serviceConnection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print('closing connection to', data.addr)
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print('echoing', repr(data.outb), 'to', data.addr)
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
 import sys
 import socket
 import selectors
@@ -43,16 +13,6 @@ else:
     sock.bind((host, int(port)))
     sock.listen(10)
     sock.setblocking(False)
-    sel = selectors.DefaultSelector()
-    sel.register(sock, selectors.EVENT_READ, data=None)
-    print('listening on', sock.getsockname())
-while True:
-    events = sel.select(timeout=None)
-    for key, mask in events:
-        if key.data is None:
-            acceptConnection(key.fileobj)
-        else:
-            serviceConnection(key, mask)
     sock.send("accio")
     if sock.recv(1024) == b'confirm-accio\r\n':
         sock.send("accio")
